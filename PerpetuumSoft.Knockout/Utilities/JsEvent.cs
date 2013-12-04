37b3d73354589cbdcde0c2a5ec09debcecee7023
Sergei Kuz'michev <ku3mich@gmail.com>
2013-12-04 18:16:19 +0200

    in progress

---
+++ PerpetuumSoft.Knockout/Utilities/JsEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerpetuumSoft.Knockout.Utilities
+{
+    public enum JsEvent
+    {
+        Click, DblClick, Change, KeyDown, KeyUp, Focus, Hover, KeyPress, MouseDown, MouseMove, MouseIn, MouseOut, MouseOver, Submit, 
+    }
+}
